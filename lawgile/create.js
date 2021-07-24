7543ad7819ba034be71399101663c9dd8c6b553b
Matheus Mereb <matheus.mereb@gmail.com>
2021-07-23 21:50:03 -0300

    Added Serverless API

--- lawgile/create.js
+++ lawgile/create.js
@@ -1,35 +1,22 @@
+import handler from "./libs/handler-lib";
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     Item: {
       // The attributes of the item to be created
       userId: "123", // The id of the author
-      noteId: uuid.v1(), // A unique uuid
+      pedidoId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
       createdAt: Date.now(), // Current Unix timestamp
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+});
